b7deb947dc78126aa88941a6e032bdc4e268d27b
hyn1992 <huangyanan@yitong111.com>
2016-07-18 14:32:55 +0800

    Initial Commit

---
+++ YTArearPickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YTArearPickerView
+//
+//  Created by Huang Yanan on 16/7/18.
+//  Copyright © 2016年 Huang Yanan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YTArearPickerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YTArearPickerView
+//
+//  Created by Huang Yanan on 16/7/18.
+//  Copyright © 2016年 Huang Yanan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
